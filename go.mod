92adbf94db14dbf7ea47774ee32c39214cfeb5a8
zerebron <>
2022-02-17 09:18:47 +0100

    Added SFDate as helper

--- go.mod
+++ go.mod
@@ -7,6 +7,8 @@ require github.com/nats-io/nats.go v1.13.1-0.20220121202836-972a071d373d
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/nats-io/nats-server/v2 v2.7.2 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 )
 
@@ -17,5 +19,6 @@ require (
 	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/nimajalali/go-force v0.0.0-20200831220737-454890ee2b7c
 	github.com/pkg/errors v0.9.1
+	github.com/vmihailenco/msgpack v4.0.4+incompatible
 	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce // indirect
 )
---
+++ helper/date.go
@@ -0,0 +1,58 @@
+package internal
+
+import (
+	"encoding/json"
+	"strings"
+	"time"
+
+	"github.com/vmihailenco/msgpack"
+)
+
+// Date is a Salesforce Date
+type Date time.Time
+
+const DateFormatSF = "2005-10-08T01:02:03Z"
+
+// Implement Unmarshaler interface
+func (t *Date) UnmarshalJSON(b []byte) error {
+	s := strings.Trim(string(b), "\"")
+	tt, err := time.Parse(DateFormatSF, s)
+	if err != nil {
+		return err
+	}
+	*t = Date(tt)
+	return nil
+}
+
+// Implement Marshaler interface
+func (t *Date) MarshalJSON() ([]byte, error) {
+	return json.Marshal(time.Time(*t))
+}
+
+func (t *Date) EncodeMsgpack(enc *msgpack.Encoder) error {
+	return enc.Encode(time.Time(*t))
+}
+
+func (t *Date) DecodeMsgpack(dec *msgpack.Decoder) error {
+	var tm time.Time
+	err := dec.Decode(&tm)
+	if err != nil {
+		return err
+	}
+	*t = Date(tm)
+	return nil
+}
+
+func (t *Date) Format(s string) string {
+	tt := time.Time(*t)
+	return tt.Format(s)
+}
+
+func (t *Date) FormatSF() string {
+	return t.Format(DateFormatSF)
+}
+
+func (t *Date) Copy() *Date {
+	out := Date(time.Time(*t).Add(0 * time.Second))
+	return &out
+}
